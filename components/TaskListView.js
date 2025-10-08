fc93448910b8cda341da2757e207aae4e63642fc
Charles C <charlesserranocarreon@gmail.com>
2025-10-08 16:53:55 +0800

    edited task view

--- components/TaskListView.js
+++ components/TaskListView.js
@@ -1,4 +1,24 @@
 import React from 'react';
 
+// Task Title, Descripition, Priority
+let tasks = [
+    ["setup", "Set up the development environment", "High"],
+    ["components", "Finish the project components", "High"]
+];
+
+function TaskListView() {
+    return (
+        <div>
+            <h2>Task List</h2>
+            <ul>
+                {tasks.map((task, index) => (
+                    <li key={index}>
+                        <strong>{task[0]}</strong>: {task[1]} (Priority: {task[2]})
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
 export default TaskListView;
